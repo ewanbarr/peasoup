d07c5a92973051a39b3e34531b3197606151b5da
ewanbarr <ewan.d.barr@googlemail.com>
2013-07-15 11:10:06 +1000

    First pass at timeseries.cpp

--- src/data_types/timeseries.cpp
+++ src/data_types/timeseries.cpp
@@ -1,29 +1,36 @@
+#include "data_types/timeseries.hpp"
 
 using namespace std;
 
 //TimeSeries definition goes here
+template <class T>
+TimeSeries::TimeSeries(T* data_ptr, unsigned int nsamps, float tsamp)
+:data_ptr(data_ptr), nsamps(nsamps), tsamp(tsamp)
+{
+}
 
+T TimeSeries::operator[](int idx)
+{
+   return data_ptr[idx];	
+}
 
-
-
-
-
-
-//TimeSeries containers go here
+template <class T>
+DedisperedTimeSeries::DedispersedTimeSeries(T* data_ptr, unsigned int nsamps, float tsamp, float dm)
+:TimeSeries<T>(data_ptr,nsamps,tsamp),dm(dm)
+{
+}
 
 template <class T>
 DispersionTrials::DispersionTrials(T* data_ptr, unsigned int nsamps, 
-				   std::vector<float> dm_list_in)
-  :data_ptr(data_ptr),
-   nsamps(nsamps),
-   count(dm_list_in.size())
+				   float tsamp, std::vector<float> dm_list_in)
+  :TimeSeriesContainer<T>(data_ptr,nsamps,tsamp,dm_list_in.size())
 {
   dm_list.swap(dm_list_in);
 }
 
 DedisperedTimeSeries DispersionTrials::operator[](int idx)
 {
-  //Form DedisperedTimeSeries instance
+   return DedisperedTimeSeries<T>(data_ptr+idx*nsamps, nsamps, tsamp, dm_list[idx]);
 }
 
 
